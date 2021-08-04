33f0dc3ab6c940e7e25c8b0126fe20c1f635acfc
seunghwan250 <ohsine2010@gmail.com>
2021-08-04 15:27:34 +0900

    0804_update_osh
    forest npc, monster spawn point

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -43,8 +43,8 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
         if(scene.buildIndex == 1)   //we're in the game scene
         {
-            PhotonNetwork.Instantiate("Player_Check", Vector3.zero, Quaternion.identity);
-            //PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);
+            //PhotonNetwork.Instantiate("Player_Check", Vector3.zero, Quaternion.identity);
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);
         }
     }
 
