4d28d52f59943c182ba113c94740411a163b4b70
hsm716 <hsm716200@naver.com>
2021-08-05 19:57:33 +0900

    Revert "Merge branch 'main' of https://github.com/hsm716/Project_ohho into main"
    This reverts commit 30e7b9319681dbae2ad5fa8a14f935d5c37aea8f, reversing
    changes made to 6272979a41d35207e9abd16dda140f552e82e09c.

--- Assets/Resources/ETC/Script/Player_Interface.cs
+++ Assets/Resources/ETC/Script/Player_Interface.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,81 +7,12 @@ public class Player_Interface : MonoBehaviour
 {
     public Player_Control player_data;
     public Slider expBar;
-    public GameObject LevelUpPanel;
-
-    public Image[] select_img;
-    //public Text[] select_name;
-    public TextMeshProUGUI[] select_name;
-
-    public Sprite[] select_ability_icons;
-    string[] select_ability_names = {"HP","SPEED","POWER","SHIELD","MP"};
+    
 
     // Update is called once per frame
-
     void Update()
     {
         expBar.value = player_data.curEXP / player_data.maxEXP;
 
-    }
-    public void Select(int index)
-    {
-        switch (select_name[index].text) 
-        {
-            case "HP":
-                HpUp();
-                break;
-            case "SPEED":
-                SpeedUp();
-                break;
-            case "POWER":
-                PowerUp();
-                break;
-            case "SHIELD":
-                break;
-            case "MP":
-                break;
-        
-        }
-        LevelUpPanel.SetActive(false);
-
-    }
-    public void HpUp()
-    {
-        player_data.maxHP += 1000;
-        player_data.Hp_Bar.GetHpBoost();
-    }
-    public void SpeedUp()
-    {
-        player_data.walkSpeed += 1;
-        player_data.curSpeed = player_data.walkSpeed;
-        player_data.animator.SetFloat("RunningAmount", player_data.animator.GetFloat("RunningAmount") + 0.2f);
-    }
-    public void PowerUp()
-    {
-        player_data.atk *= 1.25f;
-    }
-
-    IEnumerator Shuffle()
-    {
-        yield return new WaitForSeconds(1f);
-        bool[] selected_state = { false, false, false, false, false };
-
-        int count = 0;
-        while (count < 3)
-        {
-            int rand_idx = Random.Range(0, 5);
-            if (selected_state[rand_idx] == false)
-            {
-                selected_state[rand_idx] = true;
-                select_img[count].sprite = select_ability_icons[rand_idx];
-                select_name[count].text = select_ability_names[rand_idx];
-                count++;
-            }
-            else
-            {
-                continue;
-            }
-        }
-        LevelUpPanel.SetActive(true);
     }
 }
