8780f8c47016a96a7b4030ce2331aff4fd741aa9
seunghwan250 <ohsine2010@gmail.com>
2021-08-09 00:13:51 +0900

    0809_update_osh
    map

--- Assets/Asset/Scripts/MapManager.cs
+++ Assets/Asset/Scripts/MapManager.cs
@@ -6,9 +6,32 @@ public class MapManager : MonoBehaviour
 {
     public Transform Section_Offsets;
     public GameObject[] Sections;
+    public GameObject[] Sections2;
 
     void Start()
     {
+        Shuffle();
+        
+    }
+
+    void Shuffle()
+    {
+        bool[] selected_state = { false, false, false, false, false, false };
+        int count = 0;
+        while (count < 6)
+        {
+            int rand_idx = Random.Range(0, 6);
+            if (selected_state[rand_idx] == false)
+            {
+                selected_state[rand_idx] = true;
+                Sections2[rand_idx] = Sections[count];
+                count++;
+            }
+            else
+            {
+                continue;
+            }
+        }
         StartCoroutine(Call_Section());
 
     }
@@ -20,10 +43,11 @@ public class MapManager : MonoBehaviour
         foreach (Transform offset in Section_Offsets)
         {
             yield return new WaitForSeconds(3f);
-            GameObject section = Instantiate(Sections[j]);
-            section.transform.parent = offset;
-            section.transform.localPosition = new Vector3(0, 0, 0);
-            section.transform.localRotation = Quaternion.identity;
+            GameObject sections2 = Instantiate(Sections2[j]);
+            sections2.transform.GetChild(0).localRotation = Quaternion.Euler(-90, 0, -60 * j);
+            sections2.transform.parent = offset;
+            sections2.transform.localPosition = new Vector3(0, 0, 0);
+            sections2.transform.localRotation = Quaternion.identity;
             j++;
         }
     }
