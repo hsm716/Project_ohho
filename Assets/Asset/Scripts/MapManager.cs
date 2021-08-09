85d03f1ff3c0c7bb9454d6451fa4924f84f49cfb
seunghwan250 <ohsine2010@gmail.com>
2021-08-09 15:23:39 +0900

    0809_update_osh_3
    map3

--- Assets/Asset/Scripts/MapManager.cs
+++ Assets/Asset/Scripts/MapManager.cs
@@ -39,6 +39,13 @@ public class MapManager : MonoBehaviourPunCallbacks
                 continue;
             }
         }
+        PV.RPC("Shuffle_Result", RpcTarget.All);
+    }
+
+    [PunRPC]
+    void Shuffle_Result()
+    {
+        Sections = Sections2;
         StartCoroutine(Call_Section());
     }
 
@@ -49,11 +56,11 @@ public class MapManager : MonoBehaviourPunCallbacks
         foreach (Transform offset in Section_Offsets)
         {
             yield return new WaitForSeconds(3f);
-            GameObject sections2 = Instantiate(Sections2[j]);
-            sections2.transform.GetChild(0).localRotation = Quaternion.Euler(-90, 0, -60 * j);
-            sections2.transform.parent = offset;
-            sections2.transform.localPosition = new Vector3(0, 0, 0);
-            sections2.transform.localRotation = Quaternion.identity;
+            GameObject sections = Instantiate(Sections[j]);
+            sections.transform.GetChild(0).localRotation = Quaternion.Euler(-90, 0, -60 * j);
+            sections.transform.parent = offset;
+            sections.transform.localPosition = new Vector3(0, 0, 0);
+            sections.transform.localRotation = Quaternion.identity;
             j++;
         }
         StartCoroutine(Phase());
