15e662e709d0d2d079344a4e4a1b9e17e209595e
hsm716 <hsm716200@naver.com>
2021-08-20 17:27:10 +0900

    0820_update_4_hsm
    navmesh 동기화

--- Assets/NavMeshBakerTest.cs
+++ Assets/NavMeshBakerTest.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +9,15 @@ public class NavMeshBakerTest : MonoBehaviour
     public GameObject navMesh_parent;
     public NavMeshSurface[] navMeshSurface;
     public GameManager gm;
+    public PhotonView PV;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("StartMesh", 27f);
+        if(PV.IsMine)
+            Invoke("StartMesh", 27f);
     }
 
+    [PunRPC]
     void StartMesh()
     {
         navMesh_parent.transform.GetChild(0).GetComponent<NavMeshSurface>().BuildNavMesh();
